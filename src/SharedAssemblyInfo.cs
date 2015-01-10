a009cf1d4ebdcb56c8eb0fe135d384346677902e
Damian Hickey <dhickey@gmail.com>
2015-01-10 22:59:50 +0100

    Bump to version 0.3

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyProduct("Cedar.HttpCommandHandling")]
 [assembly: AssemblyCopyright("Copyright © DamianHickey 2014-215")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
