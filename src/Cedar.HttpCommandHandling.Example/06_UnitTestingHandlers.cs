682994f5a4da59d2c12a82742d76e0f07ca75f37
Damian Hickey <dhickey@gmail.com>
2015-01-11 21:12:45 +0100

    Unit testing handlers example.

---
+++ src/Cedar.HttpCommandHandling.Example/06_UnitTestingHandlers.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Example of how to test a handler
+ */
+
+// ReSharper disable once CheckNamespace
+namespace Cedar.HttpCommandHandling.Example.Commands.UnitTestinHandlers
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Cedar.HttpCommandHandling;
+    using FakeItEasy;
+    using Xunit;
+
+    public class Command
+    {
+        public string Name { get; set; }
+    }
+
+    public interface IFoo
+    {
+        Task Bar(string name);
+    }
+
+    public class CommandModule : CommandHandlerModule
+    {
+        public CommandModule(Func<IFoo> getFoo)
+        {
+            For<Command>()
+                .Handle(async (commandMessage, ct) =>
+                {
+                    var foo = getFoo();
+                    await foo.Bar(commandMessage.Command.Name);
+                });
+        }
+    }
+
+    public class CommandModuleTests
+    {
+        [Fact]
+        public void Command_should_call_service()
+        {
+            string name = "jrustle";
+            var foo = A.Fake<IFoo>();
+            A.CallTo(() => foo.Bar(name));
+            var sut = new CommandHandlerResolver(new CommandModule(() => foo));
+
+            var command = new Command { Name = name };
+            sut.Resolve<Command>()(new CommandMessage<Command>(Guid.NewGuid(), null, command),
+                CancellationToken.None);
+
+            A.CallTo(() => foo.Bar(name)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+    }
+}
