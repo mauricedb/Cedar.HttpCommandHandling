434f52ace6df7eefd2cb0f07146a3f6f2d2b9da1
Damian Hickey <dhickey@gmail.com>
2015-01-13 16:07:24 +0100

    Version 0.4.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyProduct("Cedar.HttpCommandHandling")]
-[assembly: AssemblyCopyright("Copyright © DamianHickey 2014-215")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyCopyright("Copyright © DamianHickey 2014-2015")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
