5d587d23df98cbd5de5822b5e06b108e7e931d3a
Maurice <maurice.de.beijer@gmail.com>
2015-01-28 10:28:59 +0100

    jQuery example

---
+++ src/Cedar.HttpCommandHandling.Example.JavaScript/wwwroot/jQueryExample.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+
+    $('#btnAccepted').click(function () {
+        sendCommand('Cedar.HttpCommandHandling.Example.JavaScript.CommandThatIsAccepted', { value: 'Data' });
+    });
+
+    $('#btnException').click(function () {
+        sendCommand('Cedar.HttpCommandHandling.Example.JavaScript.CommandThatThrowsProblemDetailsException', {});
+    });
+
+    function sendCommand(commandType, commandData) {
+        $.ajax({
+            url: '/commands/90D552BE-9259-4081-BEE0-A972D0AFAC8C',
+            type: 'PUT',
+            contentType: 'application/vnd.' + commandType.toLowerCase() + '+json',
+            accepts: 'application/problem+json',
+            data: JSON.stringify(commandData)
+        }).then(function (e) {
+            $('#result').text('Command Is Accepted');
+        }, function (e) {
+            console.log($('#result').text(e.responseText));
+        });;
+    }
+});
