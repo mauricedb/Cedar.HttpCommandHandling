d179cc266d6e1f3958ff5f0e43a7ca79a8452527
Maurice <maurice.de.beijer@gmail.com>
2015-01-28 10:28:32 +0100

    JavaScript example host

---
+++ src/Cedar.HttpCommandHandling.Example.JavaScript/CommandModule.cs
@@ -0,0 +1,32 @@
+﻿namespace Cedar.HttpCommandHandling.Example.JavaScript
+{
+    using System;
+    using System.Net;
+
+    public class CommandModule : CommandHandlerModule
+    {
+        public CommandModule()
+        {
+            For<CommandThatIsAccepted>().Handle(message
+                => Console.WriteLine("Command {0} with {1}", message.CommandId, message.Command.Value));
+            For<CommandThatThrowsProblemDetailsException>().Handle(_ =>
+            {
+                var details = new HttpProblemDetails
+                {
+                    Status = (int) HttpStatusCode.BadRequest,
+                    Title = "A Bad Request",
+                    Detail = "A command that is not accepted"
+                };
+                throw new HttpProblemDetailsException<HttpProblemDetails>(details);
+            });
+        }
+    }
+
+    public class CommandThatIsAccepted
+    {
+        public string Value { get; set; }
+    }
+
+    public class CommandThatThrowsProblemDetailsException
+    {}
+}
---
+++ src/Cedar.HttpCommandHandling.Example.JavaScript/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Cedar.HttpCommandHandling.Example.JavaScript
+{
+    using System;
+    using System.Diagnostics;
+    using Microsoft.Owin;
+    using Microsoft.Owin.FileSystems;
+    using Microsoft.Owin.Hosting;
+    using Microsoft.Owin.StaticFiles;
+    using Owin;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var resolver = new CommandHandlerResolver(new CommandModule());
+            var settings = new CommandHandlingSettings(resolver);
+            var commandHandlingMiddleware = CommandHandlingMiddleware.HandleCommands(settings);
+
+            using(WebApp.Start("http://localhost:8080",
+                app =>
+                {
+                    app.UseStaticFiles(new StaticFileOptions
+                    {
+                        RequestPath = new PathString(""),
+                        FileSystem = new PhysicalFileSystem(@"..\..\wwwroot")
+                    });
+                    app.Map("/commands", commandsApp => commandsApp.Use(commandHandlingMiddleware));
+                }))
+            {
+                Process.Start("http://localhost:8080/index.html");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
